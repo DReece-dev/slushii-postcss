535baa8a7d31b104e5159a3cba9aeea18c8f6077
Dylan R Hewitt <65405122+DReece-dev@users.noreply.github.com>
2022-01-20 12:13:47 +0000

    Update
    Now includes purgecss

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,32 @@
-let postcss = require('gulp-postcss');
-let gulp = require('gulp');
-let autoprefixer = require('autoprefixer');
-let cssnano = require('cssnano');
-let cssnested = require('postcss-nested');
+const postcss = require('gulp-postcss');
+const gulp = require('gulp');
+const autoprefixer = require('autoprefixer');
+const purgecss = require('gulp-purgecss');
+const cssnano = require('cssnano');
 
-gulp.task('css', function () {
+gulp.task('css', () => {
     let plugins = [
-        autoprefixer,
-        cssnano(),
-        cssnested
+        autoprefixer
     ];
     return gulp.src('./src/*.css')
         .pipe(postcss(plugins))
+        .pipe(gulp.dest('./dest'));
+});
+
+gulp.task('purgecss', () => {
+
+    return gulp.src('./src/*.css')
+        .pipe(purgecss({
+            content: ['./*.html']
+        }))
+        .pipe(gulp.dest('./dest'))
+})
+
+gulp.task('mini', () => {
+    let nano = [
+        cssnano
+    ];
+    return gulp.src('./dest/*.css')
+        .pipe(postcss(nano))
         .pipe(gulp.dest('./dest'));
 });
