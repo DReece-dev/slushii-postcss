01c19470f4e4b2622ba9b8e587be9ebaec6bde18
Dylan Reece <mrdylanreece@gmail.com>
2022-11-27 00:10:16 +0000

    Small Update

--- postcss.config.js
+++ postcss.config.js
@@ -3,13 +3,13 @@ const purgecss = require('@fullhuman/postcss-purgecss')
 module.exports = {
   plugins: [
     require('autoprefixer'),
-    purgecss({
-      safelist: [],
-      content: ['src/**/*.html'],
-      defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-    }),
-    require('cssnano')({
-      preset: 'default',
-    }),
+    // purgecss({
+    //   safelist: [],
+    //   content: ['src/**/*.html'],
+    //   defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+    // }),
+    // require('cssnano')({
+    //   preset: 'default',
+    // }),
   ]
 }
